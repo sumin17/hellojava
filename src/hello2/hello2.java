c1b95d45326faa2baecbffa900ca45acc64c7ca0
yusumin <ysm8321@gmail.com>
2021-11-16 16:56:12 +0900

    test github

---
+++ src/hello2/hello2.java
@@ -0,0 +1,16 @@
+package hello2;
+
+public class hello2 {
+
+	public static void main(String[] args) {
+		int a;
+		
+		String b = "수민";
+		a = 100;
+		System.out.println("안녕하세요");
+		System.out.println("저는 "+b+"입니다");
+		System.out.println("잘 부탁드립니다");
+
+	}
+
+}
