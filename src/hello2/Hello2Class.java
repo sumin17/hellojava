41ea37ca7cf9e5977b05462fb23eecd61910e72d
유수민 <@DESKTOP-G13MMOI>
2021-11-16 17:33:07 +0900

    class name 고침

--- src/hello2/Hello2Class.java
+++ src/hello2/Hello2Class.java
@@ -5,7 +5,7 @@ public class Hello2Class {
 	public static void main(String[] args) {
 		int a;
 		
-		String b = "수민";
+		String b = "유수민";
 		a = 100;
 		System.out.println("안녕하세요");
 		System.out.println("저는 "+b+"입니다");
