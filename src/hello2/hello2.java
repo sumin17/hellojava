f752adee83d940986412620a6c962f9312f02e82
yusumin <ysm8321@gmail.com>
2021-11-16 17:02:47 +0900

    test github

--- src/hello2/hello2.java
+++ src/hello2/hello2.java
@@ -8,7 +8,7 @@ public class hello2 {
 		String b = "수민";
 		a = 100;
 		System.out.println("안녕하세요");
-		System.out.println("저는 "+b+"입니다");
+		System.out.println("저는 "+ b +"입니다");
 		System.out.println("잘 부탁드립니다");
 
 	}
